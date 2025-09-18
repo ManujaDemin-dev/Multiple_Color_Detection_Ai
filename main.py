937bc1c1e6213256a7fe14c98b835822a1017b4e
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-09-19 00:19:07 +0530

    Main Ai_vision

--- main.py
+++ main.py
@@ -23,6 +23,60 @@ while(1):
     blue_upper = np.array([120, 255 , 255], np.uint8)
     blue_mask =cv2.inRange(hsvFrame , blue_lower , blue_upper)
 
+    kernel = np.ones((5,5), "unit8")
+
+    red_mask = cv2.dilate(red_mask, kernel)
+    res_red = cv2.bitwise_and(imageFrame , imageFrame, mask = red_mask)
+
+    green_mask = cv2.dilate(green_mask, kernel)
+    res_green = cv2.bitwise_and(imageFrame , imageFrame, mask = green_mask)
+
+    blue_mask = cv2.dilate(blue_mask, kernel)
+    res_blue = cv2.bitwise_and(imageFrame , imageFrame, mask = blue_mask)
+
+
+    #red color :) 
+    contours , hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE , cv2.CHAIN_APPROX_SIMPLE)
+
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x , y , w , h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x , y), (x + w , y + h) , (0 , 0 , 255), 2)
+
+            cv2.putText(imageFrame, "Red_detect" , (x , y) , cv2.FONT_HERSHEY_SIMPLEX , 1.0 ,(0,0,255))
+
+    #green color :)
+
+    contours , hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE , cv2.CHAIN_APPROX_SIMPLE)
+
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x , y , w , h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x , y), (x + w , y + h) , (0 , 0 , 255), 2)
+
+            cv2.putText(imageFrame, "Green_detect" , (x , y) , cv2.FONT_HERSHEY_SIMPLEX , 1.0 ,(0,0,255))
+
+    #blue color :)
+
+    contours , hierarchy = cv2.findContours(blue_mask, cv2.RETR_TREE , cv2.CHAIN_APPROX_SIMPLE)
+
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x , y , w , h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x , y), (x + w , y + h) , (0 , 0 , 255), 2)
+
+            cv2.putText(imageFrame, "Blue_detect" , (x , y) , cv2.FONT_HERSHEY_SIMPLEX , 1.0 ,(0,0,255))
+
+    
+    cv2.imshow("Multiple Color Detect Ai" , imageFrame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        webcam.release()
+        cv2.destroyAllWindows()
+        break
+
 
 
 
